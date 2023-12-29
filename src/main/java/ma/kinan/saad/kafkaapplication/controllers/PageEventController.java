529a4e9f1ed9ab560d81cdc8d52aa3ac7545886f
SR <e.saad.kinan@gmail.com>
2023-12-29 21:41:16 +0100

    analytics

--- src/main/java/ma/kinan/saad/kafkaapplication/controllers/PageEventController.java
+++ src/main/java/ma/kinan/saad/kafkaapplication/controllers/PageEventController.java
@@ -1,13 +1,24 @@
 package ma.kinan.saad.kafkaapplication.controllers;
 
 import lombok.AllArgsConstructor;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.apache.kafka.streams.state.*;
+import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ma.kinan.saad.kafkaapplication.models.PageEvent;
+import reactor.core.publisher.Flux;
 
+import java.awt.*;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 /**
@@ -17,6 +28,7 @@ import java.util.Random;
 @AllArgsConstructor
 public class PageEventController {
     private StreamBridge streamBridge;
+    private InteractiveQueryService interactiveQueryService;
     @GetMapping("/publish/{topic}/{pageName}")
     public PageEvent publish(@PathVariable String topic, @PathVariable String pageName){
         PageEvent pageEvent = new PageEvent(
@@ -26,5 +38,21 @@ public class PageEventController {
                 new Random().nextInt(9000));
         this.streamBridge.send(topic, pageEvent);
         return pageEvent;
+    }
+    @GetMapping(value = "/analytics",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Map<String,Long>> analytics(){
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(seq->{
+                    Map<String,Long> map=new HashMap<>();
+                    ReadOnlyKeyValueStore<String, Long> stats = interactiveQueryService.getQueryableStore("count-store", QueryableStoreTypes.keyValueStore());
+                    Instant now=Instant.now();
+                    Instant from=now.minusSeconds(5);
+                    KeyValueIterator<String, Long> keyValueIterator = stats.all();
+                    while (keyValueIterator.hasNext()){
+                        KeyValue<String, Long> next = keyValueIterator.next();
+                        map.put(next.key,next.value);
+                    }
+                    return map;
+                });
     }
 }
